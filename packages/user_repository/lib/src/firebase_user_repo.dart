32e1ffb99de32c119e7a6773f56233d745cbec33
sugith10 <sugithkodappurath@gmail.com>
2024-03-04 23:45:34 +0530

    errors persits in the project

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,7 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:user_repository/src/user_repo.dart';
 
 
 class FirebaseUserRepo implements UserRepository{
    final FirebaseAuth _firebaseAuth;
    final usersCollection = FirebaseFirestore.instance.collection('users');
+
+   FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+   }) = _firebaseAuth = FirebaseAuth ?? FirebaseAuth.instance;
+   
+     @override
+     Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+     }
+   
+     @override
+     Future<void> setUserData(user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+     }
+   
+     @override
+     Future<dynamic> signUp(myUser, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+     }
+   
+     @override
+     // TODO: implement user
+     Stream<dynamic> get user => throw UnimplementedError();
 }
